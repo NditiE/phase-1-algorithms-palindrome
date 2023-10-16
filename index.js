da14ba1730ebb5d23baa137c8a229320752bcb4c
MarleneElla <ellamarlene54@gmail.com>
2023-10-16 10:22:56 +0300

    done

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  
 }
 
 /* 
   Add your pseudocode here
 */
+function isPalindrome(str) {
+  // Convert to lowercase and remove spaces and special characters
+  str = str.toLowerCase().replace(/[^a-z]/g, '');
+  
+  let left = 0;
+  let right = str.length - 1;
+  
+  while (left <= right) {
+    if (str[left] !== str[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+  
+  return true;
+}
+
 
 /*
   Add written explanation of your solution here
